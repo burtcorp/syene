79a6c774223d17420fa6aaeb2e85fbc17f86f000
Theo <theo@iconara.net>
2010-08-04 12:28:48 +0200

    Path fixes

--- spec/syene/updater_spec.rb
+++ spec/syene/updater_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path('../../spec_helper', __FILE__)
 require 'tmpdir'
 require 'fileutils'
 require 'zipruby'
